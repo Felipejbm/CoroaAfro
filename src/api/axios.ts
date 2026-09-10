d053ad34e41bcae4c5ec165b4bcc273c5febce2f
FelipeJbm <felipejbmello.etec@gmail.com>
2026-09-09 23:59:38 -0300

    feat(conexao): conecta com o back em produção

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:8000",
+    baseURL: import.meta.env.VITE_API_URL ?? "https://coroaafroapi-production.up.railway.app/",
     withCredentials: true,
     timeout: 30000,
 });
